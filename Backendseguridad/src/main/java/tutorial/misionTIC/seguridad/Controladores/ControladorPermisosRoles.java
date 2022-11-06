a69fe4d3bdaca0b234d3b2cbaefd5fbe2b0d1d6f
LuchoCalderon <95188365+LuchoCalderon@users.noreply.github.com>
2022-11-06 15:45:57 -0500

    Add files via upload

---
+++ Backendseguridad/src/main/java/tutorial/misionTIC/seguridad/Controladores/ControladorPermisosRoles.java
@@ -0,0 +1,106 @@
+package tutorial.misionTIC.seguridad.Controladores;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import tutorial.misionTIC.seguridad.Modelos.Permiso;
+import tutorial.misionTIC.seguridad.Modelos.PermisosRoles;
+import tutorial.misionTIC.seguridad.Modelos.Rol;
+import tutorial.misionTIC.seguridad.Repositorios.RepositorioPermiso;
+import tutorial.misionTIC.seguridad.Repositorios.RepositorioPermisosRoles;
+import tutorial.misionTIC.seguridad.Repositorios.RepositorioRol;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/permisos-roles")
+public class ControladorPermisosRoles {
+    @Autowired
+    private RepositorioPermisosRoles miRepositorioPermisosRoles;
+    @Autowired
+    private RepositorioPermiso miRepositorioPermiso;
+    @Autowired
+    private RepositorioRol miRepositorioRol;
+
+    @GetMapping("")
+    public List<PermisosRoles> index(){
+        return this.miRepositorioPermisosRoles.findAll();
+    }
+    /**
+
+     * Asignación rol y permiso
+     * @param id_rol
+     * @param id_permiso
+     * @return
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("rol/{id_rol}/permiso/{id_permiso}")
+    public PermisosRoles create(@PathVariable String id_rol,@PathVariable String id_permiso){
+        PermisosRoles nuevo=new PermisosRoles();
+        Rol elRol=this.miRepositorioRol.findById(id_rol).get();
+        Permiso elPermiso=this.miRepositorioPermiso.findById(id_permiso).get();
+        if (elRol!=null && elPermiso!=null){
+            nuevo.setPermiso(elPermiso);
+            nuevo.setRol(elRol);
+            return this.miRepositorioPermisosRoles.save(nuevo);
+        }else{
+            return null;
+        }
+    }
+    @GetMapping("{id}")
+    public PermisosRoles show(@PathVariable String id){
+        PermisosRoles permisosRolesActual=this.miRepositorioPermisosRoles
+                .findById(id)
+                .orElse(null);
+        return permisosRolesActual;
+    }
+
+    @GetMapping("validar-permiso/rol/{id_rol}")
+    public PermisosRoles getPermiso(@PathVariable String id_rol,@RequestBody Permiso infoPermiso)
+    {
+        Permiso elPermiso=this.miRepositorioPermiso.getPermiso(infoPermiso.getUrl(),infoPermiso.getMetodo());
+        Rol elRol=this.miRepositorioRol
+                .findById(id_rol)
+                .orElse(null);
+        if (elPermiso!=null && elRol!=null){
+            return this.miRepositorioPermisosRoles.getPermisos(elRol.get_id(),elPermiso.get_id());
+        }else{
+            return null;
+        }
+    }
+    /**
+     * Modificación Rol y Permiso
+     * @param id
+     * @param id_rol
+     * @param id_permiso
+     * @return
+     */
+    @PutMapping("{id}/rol/{id_rol}/permiso/{id_permiso}")
+    public PermisosRoles update(@PathVariable String id,@PathVariable String id_rol,@PathVariable String id_permiso){
+        PermisosRoles permisosRolesActual=this.miRepositorioPermisosRoles
+                .findById(id)
+                .orElse(null);
+        Rol elRol=this.miRepositorioRol
+                .findById(id_rol)
+                .orElse(null);
+        Permiso elPermiso=this.miRepositorioPermiso.findById(id_permiso).get();
+        if(permisosRolesActual!=null && elPermiso!=null && elRol!=null){
+            permisosRolesActual.setPermiso(elPermiso);
+            permisosRolesActual.setRol(elRol);
+            return this.miRepositorioPermisosRoles.save(permisosRolesActual);
+        }else{
+            return null;
+        }
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        PermisosRoles permisosRolesActual=this.miRepositorioPermisosRoles
+                .findById(id)
+                .orElse(null);
+        if (permisosRolesActual!=null){
+            this.miRepositorioPermisosRoles.delete(permisosRolesActual);
+        }
+    }
+
+
+}
